bffb021cc1b83fe8513f98672c7f176a2295753f
Ben Amos <flashguardian13@gmail.com>
2023-12-22 18:29:54 -0800

    chat lists connected peers

--- chat.gd
+++ chat.gd
@@ -1,5 +1,27 @@
 extends Window
 
+var participants:Dictionary = {}
+
+func participant_spawner() -> MultiplayerSpawner:
+	return $ChatVBoxContainer/UpperHBoxContainer/ParticipantsScrollContainer2/MultiplayerSpawner
+
+func _ready():
+	participant_spawner().spawn_function = _spawn_partipant
+
+func _spawn_partipant(data:Dictionary) -> Label:
+	print("[%s] _spawn_partipant called with %s" % [multiplayer.get_unique_id(), data])
+	var p:Label = Label.new()
+	participants[data["peer_id"]] = p
+	p.text = data["name"]
+	return p
+
+func _delete_participant(peer_id:int) -> void:
+	print("[%s] _delete_participant called with %s" % [multiplayer.get_unique_id(), peer_id])
+	var p:Label = participants[peer_id]
+	participants.erase(p)
+	participant_spawner().get_node(participant_spawner().spawn_path).remove_child(p)
+	p.queue_free()
+
 func _on_close_requested():
 	hide()
 	get_tree().paused = false
--- game.gd
+++ game.gd
@@ -3,16 +3,17 @@ extends MarginContainer
 @export
 var player_scene:PackedScene
 
+# Network configuration
 const IP_ADDRESS:String = "127.0.0.1"
 const PORT:int = 45367
 const MAX_CLIENTS:int = 8
 
-# Map of peer IDs to players
-var players:Dictionary = {}
-
-# Player information
-var player_info:Dictionary = { "name": "Player %s" % randi() }
+# Per-peer information
+var player_details:Dictionary = {}
 var player_sprites:Dictionary = {}
+
+# Local Player information
+var my_info:Dictionary = { "name": "Player %s" % randi() }
 
 func _ready():
 	multiplayer.peer_connected.connect(_on_peer_connected)
@@ -25,96 +26,129 @@ func _ready():
 # Button press event listeners
 
 func _on_host_button_pressed():
+	print("[%s] (Click) Host" % my_peer_id())
 	if is_multiplayer():
-		print("[%s] Connection already exists." % multiplayer.get_unique_id())
+		print("[%s] (Click) Connection already exists." % my_peer_id())
 		return
 	var peer:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 	var error = peer.create_server(PORT, MAX_CLIENTS)
 	if error:
-		print("[%s] %s" % [peer.get_unique_id(), error])
+		print("[%s] (Click) %s" % [my_peer_id(), error])
 		return error
 	multiplayer.multiplayer_peer = peer
-	print("[%s] Peer (server) created." % peer.get_unique_id())
-	$MultiplayerSpawner.spawn({ "auth_id": peer.get_unique_id() })
+	print("[%s] peer is now %s" % [my_peer_id(), multiplayer.multiplayer_peer])
+	setup_player(multiplayer.get_unique_id(), my_info)
 
 func _on_join_button_pressed():
+	print("[%s] (Click) Join" % my_peer_id())
 	if is_multiplayer():
-		print("[%s] Connection already exists." % multiplayer.get_unique_id())
+		print("[%s] (Click) Connection already exists." % my_peer_id())
 		return
 	var peer:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 	var error = peer.create_client(IP_ADDRESS, PORT)
 	if error:
-		print("[%s] %s" % [peer.get_unique_id(), error])
+		print("[%s] (Click) %s" % [my_peer_id(), error])
 		return error
 	multiplayer.multiplayer_peer = peer
-	print("[%s] Peer (client) created." % peer.get_unique_id())
+	print("[%s] peer is now %s" % [my_peer_id(), multiplayer.multiplayer_peer])
 
 func _on_leave_button_pressed():
-	var peer_id:int = multiplayer.get_unique_id()
+	print("[%s] (Click) Leave" % my_peer_id())
 	if !is_multiplayer():
-		print("[%s] No connection exists." % peer_id)
+		print("[%s] (Click) No connection exists." % my_peer_id())
 		return
 	disconnect_from_server()
+
+func _on_chat_toggle_button_pressed():
+	print("[%s] (Click) Chat" % my_peer_id())
+	get_tree().paused = true
+	$ChatVBoxContainer.popup_centered()
 
 # Local multiplayer event listeners.
 
 func _on_peer_connected(id:int):
-	print("[%s] Peer %s has connected." % [multiplayer.get_unique_id(), id])
-	_register_player.rpc_id(id, player_info) # Hello, [id], here is my player info.
+	print("[%s] (Event) Peer %s has connected." % [my_peer_id(), id])
+	_register_player.rpc_id(id, my_info) # Hello, [id], here is my player info.
 
 func _on_peer_disconnected(id:int):
-	print("[%s] Peer %s disconnected." % [multiplayer.get_unique_id(), id])
-	if multiplayer.is_server():
-		despawn_player(id)
-	players.erase(id)
+	print("[%s] (Event) Peer %s disconnected." % [my_peer_id(), id])
+	teardown_player(id)
 
 func _on_connected_to_server():
-	print("[%s] Connection to server succeeded." % multiplayer.get_unique_id())
-	players[multiplayer.get_unique_id()] = player_info
+	print("[%s] (Event) Connection to server succeeded." % my_peer_id())
+	player_details[my_peer_id()] = my_info
 
 func _on_connection_failed():
-	print("[%s] Connection to server failed!" % multiplayer.get_unique_id())
+	print("[%s] (Event) Connection to server failed!" % my_peer_id())
 	disconnect_from_server()
 
 func _on_server_disconnected():
-	print("[%s] Disconnected from server." % multiplayer.get_unique_id())
+	print("[%s] (Event) Disconnected from server." % my_peer_id())
 	disconnect_from_server()
 
 # Multiplayer functions
 
 func is_multiplayer() -> bool:
-	return players.size() > 0
+	return player_details.size() > 0
+
+func my_peer_id() -> int:
+	if multiplayer && multiplayer.has_multiplayer_peer():
+		return multiplayer.get_unique_id()
+	return -1
 
 @rpc("any_peer", "reliable")
 func _register_player(player_info:Dictionary):
 	var sender_id:int = multiplayer.get_remote_sender_id()
-	print("[%s] Received information for player %s." % [multiplayer.get_unique_id(), sender_id])
-	players[sender_id] = player_info
-	if multiplayer.is_server():
-		$MultiplayerSpawner.spawn({ "auth_id": sender_id })
+	print("[%s] (RPC) Received information for player %s." % [my_peer_id(), sender_id])
+	setup_player(sender_id, player_info)
 
-func disconnect_from_server() -> void:
-	players.clear()
-	print("[%s] Peer destroyed." % multiplayer.get_unique_id())
-	multiplayer.multiplayer_peer = null
+func setup_player(peer_id:int, peer_info:Dictionary):
+	print("[%s] setup_player() called for peer %s." % [my_peer_id(), peer_id])
+	player_details[peer_id] = peer_info
+	if multiplayer.is_server():
+		$MultiplayerSpawner.spawn({ "auth_id": peer_id })
+		$ChatVBoxContainer.participant_spawner().spawn(
+			{
+				"name": peer_info["name"],
+				"peer_id": peer_id
+			}
+		)
 
 func spawn_player(data:Dictionary) -> Node2D:
 	var auth_id:int = data.get("auth_id")
+	print("[%s] spawn_player() called for peer %s." % [my_peer_id(), auth_id])
 	var player_sprite:Node2D = player_scene.instantiate()
 	player_sprites[auth_id] = player_sprite
 	player_sprite.name = "Player%s" % auth_id
-	print("[%s] spawned player sprite with name '%s'" % [multiplayer.get_unique_id(), player_sprite.name])
 	player_sprite.call_deferred("set_multiplayer_authority", auth_id)
-	print("[%s] will set multiplayer authority of %s to %s" % [multiplayer.get_unique_id(), player_sprite.name, auth_id])
-	player_sprite.position = Vector2(100, 100)
+	player_sprite.position = Vector2(
+		randi() % DisplayServer.window_get_size().x,
+		randi() % DisplayServer.window_get_size().y
+	)
 	return player_sprite
 
+func disconnect_from_server() -> void:
+	if !is_multiplayer():
+		return
+	print("[%s] disconnect_from_server() called." % my_peer_id())
+	var old_peer:MultiplayerPeer = multiplayer.multiplayer_peer
+	multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new()
+	print("[%s] peer is now %s" % [my_peer_id(), multiplayer.multiplayer_peer])
+	old_peer.close()
+	print("[%s] Multiplayer peer closed." % my_peer_id())
+	for player_id in player_details.keys():
+		teardown_player(player_id)
+
+func teardown_player(peer_id:int):
+	print("[%s] teardown_player() called for peer %s." % [my_peer_id(), peer_id])
+	player_details.erase(peer_id)
+	if multiplayer.is_server():
+		despawn_player(peer_id)
+		$ChatVBoxContainer._delete_participant(peer_id)
+
 func despawn_player(id:int) -> void:
+	print("[%s] despawn_player() called for peer %s." % [my_peer_id(), id])
 	var player_sprite:Node2D = player_sprites[id]
 	player_sprites.erase(id)
 	$World.remove_child(player_sprite)
 	player_sprite.queue_free()
-
-func _on_chat_toggle_button_pressed():
-	get_tree().paused = true
-	$ChatVBoxContainer.popup_centered()
